93ab16478b3be8592b5856d7ef603484da7a13ba
Gina Antonini <ginamantonini@gmail.com>
2017-09-07 20:09:12 -0500

    added the array to get the selected meats

--- SandwichMaker.js
+++ SandwichMaker.js
@@ -1,14 +1,32 @@
 console.log("hello")
 
 
-let theBread = SandwichMaker.getBread();
-let theCheese = SandwichMaker.getCheese();
-let theCondiments = SandwichMaker.getCondiments();
-let theMeat = SandwichMaker.getMeat();
-let theVeggies = SandwichMaker.getVeggies();
+// let theBread = SandwichMaker.getBread();
+// let theCheese = SandwichMaker.getCheese();
+// let theCondiments = SandwichMaker.getCondiments();
+// let theMeat = SandwichMaker.getMeat();
+// let theVeggies = SandwichMaker.getVeggies();
 
-I need a dom string here
+chooseBread = document.getElementById("bread");
+chooseCheese = document.getElementById("cheese");
+chooseCondiments = document.getElementById("condiments");
+chooseMeat = document.getElementById("meats");
+chooseVeggies = document.getElementById("veggies")
 
-and an empty array
 
-click event to add to array 
+
+// click event to add to array 
+
+chooseMeat.addEventListener("change", function(event){
+	console.log(event);
+	if(event.target.checked === true) {
+		SandwichMaker.addToMeatArray(event.target.name);
+		SandwichMaker.showMeatArray();
+		console.log(SandwichMaker.showMeatArray());
+    } else {
+    	SandwichMaker.removeUncheckedMeats(event.target.name);
+    }
+});
+
+
+// .reduce to show the 
